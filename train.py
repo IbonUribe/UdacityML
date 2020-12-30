7d8073f2aa06cb51ec84b952f996ba5f77e34b60
Ibon Uribe <69908503+IbonUribe@users.noreply.github.com>
2020-12-30 17:34:22 +0000

    Update train.py

--- train.py
+++ train.py
@@ -9,31 +9,32 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-
-# TODO: Create TabularDataset using TabularDatasetFactory
+# Inserted includes.
+from azureml.core import Dataset
+from azureml.data.datapath import DataPath'
+# TODO: Create TabularDataset using 
 # Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
+datastore_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+ds = Dataset.Tabular.from_delimited_files(path=datastore_path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
+### YOUR CODE HERE ###
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+run = Run.get_context() # Gets the context to make experiments on Aure Machine Learning
 
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
 
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
+    # Clean and one hot encode data with "job" prefix
+    x_df = data.to_pandas_dataframe().dropna() # Remo
+    jobs = pd.get_dummies(x_df.job, prefix="job") # Convert job columns into binary "onehot" type codification with "job" prefix.
+    x_df.drop("job", inplace=True, axis=1) # Drops a column from the Data Frame.
+    x_df = x_df.join(jobs) # Joins columns from another Data Frame.job 
     x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
     x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
     x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
