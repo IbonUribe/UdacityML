366f40994634c7ee126d7f3b58bc3c7f9a02dcbd
Ibon Uribe <ibon.uribe@araia.eus>
2021-05-21 06:38:12 +0200

    Pequeños cambios

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ x, y = clean_data(ds)
 ### YOUR CODE HERE ###
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
-run = Run.get_context() # Gets the context to make experiments on Aure Machine Learning
+run = Run.get_context() # Gets the context to make experiments on Azure Machine Learning
 
 def clean_data(data):
     # Dict for cleaning data
